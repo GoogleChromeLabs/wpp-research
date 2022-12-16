d0417c6463dd3b7ac9e6324fa298bc6860054a0a
Felix Arntz <felixarntz@google.com>
2022-12-16 10:37:00 -0800

    Add link to query results in SQL file.

--- sql/2022/12/usage-of-core-themes-with-jquery.sql
+++ sql/2022/12/usage-of-core-themes-with-jquery.sql
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# See query results here: https://github.com/GoogleChromeLabs/wpp-research/pull/13
 SELECT
   _TABLE_SUFFIX AS client,
   app,
