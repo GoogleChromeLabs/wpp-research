9dd59e957ea90264a18af3941a717989e40311a6
vishalkakadiya <vishal.kakadiya@rtcamp.com>
2023-01-03 13:59:36 +0530

    Update license details

--- sql/2022/12/opportunity-score-wp-sites-assess-impact-of-inaccurate-sizes-attribute.sql
+++ sql/2022/12/opportunity-score-wp-sites-assess-impact-of-inaccurate-sizes-attribute.sql
@@ -1,6 +1,21 @@
 #standardSQL
 # measure the impact of innacurate sizes attributes per <img> for WordPress sites.
+#
+# WPP Research, Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+# See query results here: https://github.com/GoogleChromeLabs/wpp-research/pull/19
 CREATE TEMPORARY FUNCTION getSrcsetSizesAccuracy(payload STRING)
 RETURNS ARRAY<STRUCT<sizesAbsoluteError INT64, sizesRelativeError FLOAT64, wDescriptorAbsoluteError INT64, wDescriptorRelativeError FLOAT64, actualSizesEstimatedWastedLoadedPixels INT64, actualSizesEstimatedWastedLoadedBytes FLOAT64, wastedLoadedPercent FLOAT64>>
 LANGUAGE js AS '''
