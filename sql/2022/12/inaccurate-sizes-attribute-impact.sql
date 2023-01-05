7c5e59e48ca2e330cf89cfeaacba5be51d4af068
vishalkakadiya <vishal.kakadiya@rtcamp.com>
2023-01-05 18:18:56 +0530

    Fix indentation

--- sql/2022/12/inaccurate-sizes-attribute-impact.sql
+++ sql/2022/12/inaccurate-sizes-attribute-impact.sql
@@ -54,38 +54,37 @@ try {
 ''';
 
 SELECT
-  percentile,
-  client,
-	APPROX_QUANTILES(image.sizesAbsoluteError, 1000)[OFFSET(percentile * 10)] AS sizesAbsoluteError,
-	APPROX_QUANTILES(image.sizesRelativeError, 1000)[OFFSET(percentile * 10)] AS sizesRelativeError,
-	APPROX_QUANTILES(image.wDescriptorAbsoluteError, 1000)[OFFSET(percentile * 10)] AS wDescriptorAbsoluteError,
-	APPROX_QUANTILES(image.wDescriptorRelativeError, 1000)[OFFSET(percentile * 10)] AS wDescriptorRelativeError,
-	APPROX_QUANTILES(image.actualSizesEstimatedWastedLoadedPixels, 1000)[OFFSET(percentile * 10)] AS actualSizesEstimatedWastedLoadedPixels,
-	APPROX_QUANTILES(image.actualSizesEstimatedWastedLoadedBytes, 1000)[OFFSET(percentile * 10)] AS actualSizesEstimatedWastedLoadedBytes,
-	APPROX_QUANTILES(image.wastedLoadedPercent, 1000)[OFFSET(percentile * 10)] AS wastedLoadedPercent
+    percentile,
+    client,
+    APPROX_QUANTILES(image.sizesAbsoluteError, 1000)[OFFSET(percentile * 10)] AS sizesAbsoluteError,
+    APPROX_QUANTILES(image.sizesRelativeError, 1000)[OFFSET(percentile * 10)] AS sizesRelativeError,
+    APPROX_QUANTILES(image.wDescriptorAbsoluteError, 1000)[OFFSET(percentile * 10)] AS wDescriptorAbsoluteError,
+    APPROX_QUANTILES(image.wDescriptorRelativeError, 1000)[OFFSET(percentile * 10)] AS wDescriptorRelativeError,
+    APPROX_QUANTILES(image.actualSizesEstimatedWastedLoadedPixels, 1000)[OFFSET(percentile * 10)] AS actualSizesEstimatedWastedLoadedPixels,
+    APPROX_QUANTILES(image.actualSizesEstimatedWastedLoadedBytes, 1000)[OFFSET(percentile * 10)] AS actualSizesEstimatedWastedLoadedBytes,
+    APPROX_QUANTILES(image.wastedLoadedPercent, 1000)[OFFSET(percentile * 10)] AS wastedLoadedPercent
 FROM (
     SELECT
-		tpages._TABLE_SUFFIX AS client,
-		image
-	FROM
-		`httparchive.pages.2022_10_01_*` AS tpages,
-		UNNEST(getSrcsetSizesAccuracy(payload)) AS image
-	JOIN
-		`httparchive.technologies.2022_10_01_*` AS tech
-	ON
-		tech.url = tpages.url
-	WHERE
-		tpages._TABLE_SUFFIX = tech._TABLE_SUFFIX
-	AND
-		app = 'WordPress'
-	AND
-		category = 'CMS'
-	),
-	UNNEST([10, 25, 50, 75, 90]) AS percentile
+        tpages._TABLE_SUFFIX AS client,
+        image
+    FROM
+        `httparchive.pages.2022_10_01_*` AS tpages,
+        UNNEST(getSrcsetSizesAccuracy(payload)) AS image
+    JOIN
+        `httparchive.technologies.2022_10_01_*` AS tech
+    ON
+        tech.url = tpages.url
+    WHERE
+        tpages._TABLE_SUFFIX = tech._TABLE_SUFFIX
+    AND
+        app = 'WordPress'
+    AND
+        category = 'CMS'
+    ),
+    UNNEST([10, 25, 50, 75, 90]) AS percentile
 GROUP BY
-	percentile,
-	client
+    percentile,
+    client
 ORDER BY
-	percentile,
-	client
-
+    percentile,
+    client
