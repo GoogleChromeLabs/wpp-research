13ade6b79dd3655b1b41bc3c0260571adff7105d
vishalkakadiya <vishal.kakadiya@rtcamp.com>
2023-01-05 18:12:02 +0530

    Fix indentation

--- sql/2022/12/inaccurate-sizes-attribute-impact.sql
+++ sql/2022/12/inaccurate-sizes-attribute-impact.sql
@@ -65,24 +65,24 @@ SELECT
 	APPROX_QUANTILES(image.actualSizesEstimatedWastedLoadedBytes, 1000)[OFFSET(percentile * 10)] AS actualSizesEstimatedWastedLoadedBytes,
 	APPROX_QUANTILES(image.wastedLoadedPercent, 1000)[OFFSET(percentile * 10)] AS wastedLoadedPercent
 FROM (
-		SELECT
-			tpages._TABLE_SUFFIX AS client,
-			image
-		FROM
-			`httparchive.pages.2022_10_01_*` AS tpages,
-			UNNEST(getSrcsetSizesAccuracy(payload)) AS image
-				JOIN
-			`httparchive.technologies.2022_10_01_*` AS tech
-			ON
-				tech.url = tpages.url
-		WHERE
-			tpages._TABLE_SUFFIX = tech._TABLE_SUFFIX
-		AND
-			app = 'WordPress'
-		AND
-			category = 'CMS'
-	 ),
-	 UNNEST([10, 25, 50, 75, 90]) AS percentile
+	SELECT
+		tpages._TABLE_SUFFIX AS client,
+		image
+	FROM
+		`httparchive.pages.2022_10_01_*` AS tpages,
+		UNNEST(getSrcsetSizesAccuracy(payload)) AS image
+			JOIN
+		`httparchive.technologies.2022_10_01_*` AS tech
+		ON
+			tech.url = tpages.url
+	WHERE
+		tpages._TABLE_SUFFIX = tech._TABLE_SUFFIX
+	AND
+		app = 'WordPress'
+	AND
+		category = 'CMS'
+	),
+	UNNEST([10, 25, 50, 75, 90]) AS percentile
 GROUP BY
 	percentile,
 	client
