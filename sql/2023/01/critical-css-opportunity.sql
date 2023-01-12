62ff921714cc355e961fdf929ab40ff1904d35f3
Karthik Thayyil <karthik.thayyil@get10up.com>
2023-01-12 13:48:40 +0000

    modification based on some assumptions

--- sql/2023/01/critical-css-opportunity.sql
+++ sql/2023/01/critical-css-opportunity.sql
@@ -15,19 +15,6 @@
 # limitations under the License.
 
 # See query results here: https://github.com/GoogleChromeLabs/wpp-research/pull/16
-CREATE TEMP FUNCTION
-  getRenderBlockingCSSCount(lighthouse STRING)
-  RETURNS INT64
-  LANGUAGE js AS '''
-try {
-const report = JSON.parse(lighthouse);
-const networkItems = report.audits["network-requests"].details.items
-const renderBlockingCss = networkItems.filter(item => item.resourceType == "Stylesheet" && item.priority == "VeryHigh");
-return renderBlockingCss.length
-} catch (e) {
-return -1;
-}
-''';
 SELECT
   client,
   total_wp_sites,
@@ -36,27 +23,13 @@ SELECT
   CONCAT(ROUND((total_wp_sites-sites_with_critical_css)*100/total_wp_sites, 3),' %') AS opportunity
 FROM (
   SELECT
-    ap.client,
-    COUNT(ap.page) AS total_wp_sites,
-    COUNTIF( getRenderBlockingCSSCount(lighthouse) = 0
-      AND IFNULL( REGEXP_CONTAINS( response_body, '<head.+<style(.+)/style>.+/head>' ), FALSE ) ) AS sites_with_critical_css
+    pages._TABLE_SUFFIX AS client,
+    COUNT(pages.url) AS total_wp_sites,
+    COUNTIF( CAST(JSON_EXTRACT_SCALAR(payload, '$._renderBlockingCSS') AS INT64) = 0
+      AND CAST(JSON_EXTRACT_SCALAR(payload, '$._inline_style_bytes') AS INT64) > 0 ) AS sites_with_critical_css
   FROM
-    `httparchive.all.pages` ap,
-    UNNEST(technologies) AS technologies,
-    UNNEST(technologies.categories) AS category
-  JOIN
-    `httparchive.all.requests` ar
-  ON
-    ap.page = ar.page
-    AND ap.date = ar.date
-    AND ap.client = ar.client
+    `httparchive.pages.2022_10_01_*` AS pages
   WHERE
-    ap.is_root_page = TRUE
-    AND ar.is_root_page = TRUE
-    AND category = "CMS"
-    AND technologies.technology = "WordPress"
-    AND REGEXP_CONTAINS(JSON_EXTRACT(lighthouse,'$.audits.network-requests.details.items' ), '"priority"')
-    AND ap.date = "2022-11-01"
+    JSON_EXTRACT(pages.payload, '$._detected_apps.WordPress') IS NOT NULL
   GROUP BY
-    client,
-    response_body )
+    pages._TABLE_SUFFIX )
