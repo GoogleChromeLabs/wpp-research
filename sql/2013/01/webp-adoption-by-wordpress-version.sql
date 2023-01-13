605db7d54cbced945bb9f977c62424364994252c
Adam Silverstein <adamjs@google.com>
2023-01-13 14:55:35 -0700

    Re-auto-format query with Biq Query GCP console

--- sql/2013/01/webp-adoption-by-wordpress-version.sql
+++ sql/2013/01/webp-adoption-by-wordpress-version.sql
@@ -16,119 +16,107 @@
 
 # See https://github.com/GoogleChromeLabs/wpp-research/pull/17
 
-SELECT
+SSELECT
   mobile.version,
   ROUND(pct_webp_mobile, 3) AS pct_webp_mobile,
   ROUND(pct_webp_desktop, 3) AS pct_webp_desktop
-FROM
-  (
+FROM (
+  SELECT
+    version,
+    pct_webp AS pct_webp_mobile
+  FROM (
     SELECT
-      version, pct_webp AS pct_webp_mobile
-    FROM
-      (
+      version,
+      COUNTIF(has_webp) AS pages_with_webp,
+      COUNT(0) AS pages,
+      COUNTIF(has_webp) / COUNT(0) AS pct_webp
+    FROM (
+      SELECT
+        DISTINCT url,
+        REGEXP_EXTRACT(info, r'(\d\.\d+)') AS version
+      FROM
+        `httparchive.technologies.2022_10_01_mobile`
+      WHERE
+        app = 'WordPress' )
+    JOIN (
+      SELECT
+        url,
+        has_webp
+      FROM (
         SELECT
-          version,
-          COUNTIF(has_webp) AS pages_with_webp,
-          COUNT(0) AS pages,
-          COUNTIF(has_webp) / COUNT(0) AS pct_webp
+          pageid,
+          COUNTIF(ext = 'webp') > 0 AS has_webp
         FROM
-          (
-            SELECT DISTINCT
-              url,
-              REGEXP_EXTRACT(info, r'(\d\.\d+)') AS version
-            FROM
-              `httparchive.technologies.2022_10_01_mobile`
-            WHERE
-              app = 'WordPress'
-          )
-        JOIN
-          (
-            SELECT
-              url,
-              has_webp
-            FROM
-              (
-                SELECT
-                  pageid,
-                  COUNTIF(ext = 'webp') > 0 AS has_webp
-                FROM
-                  `httparchive.summary_requests.2022_10_01_mobile`
-                GROUP BY
-                  pageid
-              )
-            JOIN
-              (
-                SELECT
-                  pageid,
-                  url
-                FROM
-                  `httparchive.summary_pages.2022_10_01_mobile`
-              )
-              USING (pageid)
-          )
-          USING (url)
-        WHERE version IS NOT NULL
+          `httparchive.summary_requests.2022_10_01_mobile`
         GROUP BY
-          version
-        ORDER BY
-          version ASC
-      )
-    WHERE pages > 1500
-  ) AS mobile
-JOIN
-  (
+          pageid )
+      JOIN (
+        SELECT
+          pageid,
+          url
+        FROM
+          `httparchive.summary_pages.2022_10_01_mobile` )
+      USING
+        (pageid) )
+    USING
+      (url)
+    WHERE
+      version IS NOT NULL
+    GROUP BY
+      version
+    ORDER BY
+      version ASC )
+  WHERE
+    pages > 1500 ) AS mobile
+JOIN (
+  SELECT
+    version,
+    pct_webp AS pct_webp_desktop
+  FROM (
     SELECT
-      version, pct_webp AS pct_webp_desktop
-    FROM
-      (
+      version,
+      COUNTIF(has_webp) AS pages_with_webp,
+      COUNT(0) AS pages,
+      COUNTIF(has_webp) / COUNT(0) AS pct_webp
+    FROM (
+      SELECT
+        DISTINCT url,
+        REGEXP_EXTRACT(info, r'(\d\.\d+)') AS version
+      FROM
+        `httparchive.technologies.2022_10_01_desktop`
+      WHERE
+        app = 'WordPress' )
+    JOIN (
+      SELECT
+        url,
+        has_webp
+      FROM (
         SELECT
-          version,
-          COUNTIF(has_webp) AS pages_with_webp,
-          COUNT(0) AS pages,
-          COUNTIF(has_webp) / COUNT(0) AS pct_webp
+          pageid,
+          COUNTIF(ext = 'webp') > 0 AS has_webp
         FROM
-          (
-            SELECT DISTINCT
-              url,
-              REGEXP_EXTRACT(info, r'(\d\.\d+)') AS version
-            FROM
-              `httparchive.technologies.2022_10_01_desktop`
-            WHERE
-              app = 'WordPress'
-          )
-        JOIN
-          (
-            SELECT
-              url,
-              has_webp
-            FROM
-              (
-                SELECT
-                  pageid,
-                  COUNTIF(ext = 'webp') > 0 AS has_webp
-                FROM
-                  `httparchive.summary_requests.2022_10_01_desktop`
-                GROUP BY
-                  pageid
-              )
-            JOIN
-              (
-                SELECT
-                  pageid,
-                  url
-                FROM
-                  `httparchive.summary_pages.2022_10_01_desktop`
-              )
-              USING (pageid)
-          )
-          USING (url)
-        WHERE version IS NOT NULL
+          `httparchive.summary_requests.2022_10_01_desktop`
         GROUP BY
-          version
-        ORDER BY
-          version ASC
-      )
-    WHERE pages > 1500
-  ) AS desktop
-  ON mobile.version = desktop.version
-  ORDER BY mobile.version DESC
+          pageid )
+      JOIN (
+        SELECT
+          pageid,
+          url
+        FROM
+          `httparchive.summary_pages.2022_10_01_desktop` )
+      USING
+        (pageid) )
+    USING
+      (url)
+    WHERE
+      version IS NOT NULL
+    GROUP BY
+      version
+    ORDER BY
+      version ASC )
+  WHERE
+    pages > 1500 ) AS desktop
+ON
+  mobile.version = desktop.version
+ORDER BY
+  mobile.version DESC
