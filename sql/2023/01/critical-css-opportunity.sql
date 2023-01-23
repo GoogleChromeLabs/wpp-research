2a24b818f92d0bf4b281f9d6082a84c3cc133d54
Karthik Thayyil <30643833+kt-12@users.noreply.github.com>
2023-01-23 23:58:28 +0000

    change year licence
    Co-authored-by: Felix Arntz <felixarntz@users.noreply.github.com>

--- sql/2023/01/critical-css-opportunity.sql
+++ sql/2023/01/critical-css-opportunity.sql
@@ -1,6 +1,6 @@
 # HTTP Archive query to get % of WordPress sites not having Critical CSS implementation.
 #
-# WPP Research, Copyright 2022 Google LLC
+# WPP Research, Copyright 2023 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
