584ea2347bc0724e68aea027f44944f18c2bd0c5
Weston Ruter <westonruter@google.com>
2023-07-28 14:41:49 -0700

    Use single quotes

--- cli/commands/benchmark-web-vitals.mjs
+++ cli/commands/benchmark-web-vitals.mjs
@@ -78,7 +78,7 @@ export async function handler( opt ) {
 	const { number: amount } = opt;
 	const results = [];
 
-	const browser = await puppeteer.launch({ headless: "new" });
+	const browser = await puppeteer.launch({ headless: 'new' });
 
 	for await ( const url of getURLs( opt ) ) {
 		const { completeRequests, metrics } = await benchmarkURL( browser, {
