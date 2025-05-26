ccfb292b8b886e90dbe8e2e34fa383a072ea8be2
Weston Ruter <weston@ruter.net>
2025-05-26 09:08:09 -0700

    Simulate click at 0,0 since clicking off-screen causes error in Firefox

--- cli/commands/benchmark-web-vitals.mjs
+++ cli/commands/benchmark-web-vitals.mjs
@@ -646,7 +646,7 @@ async function benchmarkURL( url, metricsDefinition, params, logProgress ) {
 							 * Click off screen to prevent clicking a link by accident and navigating away.
 							 */
 							await page.click( 'body', {
-								offset: { x: -500, y: -500 },
+								offset: { x: 0, y: 0 },
 							} );
 							// Get the metric value from the global.
 							const metric =
