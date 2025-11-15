35e526e10c05d12d9d77e741a97bae6a4217e4d6
Weston Ruter <weston@ruter.net>
2025-11-14 21:08:50 -0800

    Avoid waiting until networkidle0 when cache is not enabled (when priming)

--- cli/commands/benchmark-web-vitals.mjs
+++ cli/commands/benchmark-web-vitals.mjs
@@ -543,7 +543,7 @@ async function benchmarkURL( url, metricsDefinition, params, logProgress ) {
 			const urlObj = new URL( url );
 			urlObj.searchParams.append( 'rnd', String( Math.random() ) );
 			await page.goto( urlObj.toString(), {
-				waitUntil: 'networkidle0',
+				waitUntil: params.enableCache ? 'networkidle0' : 'domcontentloaded',
 			} );
 			if ( params.pauseDuration ) {
 				await new Promise( ( resolve ) => {
